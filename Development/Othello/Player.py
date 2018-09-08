23c8635efc0ad4dc37130d7ec968231163a9e5dd
epnigma <s162345@student.dhbw-mannheim.de>
2018-09-08 16:45:25 +0200

    Removed untracked files

--- Development/Othello/Player.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-class Player(ABC):
-    @abstractmethod
-    def __init__(self, game_reference):
-        pass
-
-    @abstractmethod
-    def play(self):
-        pass
