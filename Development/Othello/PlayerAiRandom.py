b0d1d1e0c003ed16dfa34dc5cccd8c15dfda79ac
epnigma <s162345@student.dhbw-mannheim.de>
2018-09-09 15:21:27 +0200

    Added AI choosing random position

---
+++ Development/Othello/PlayerAiRandom.py
@@ -0,0 +1,14 @@
+from Player import Player
+import random
+
+
+class PlayerAiRandom(Player):
+    def __init__(self, game_reference):
+        self._game_reference = game_reference
+        print("Created new Human Player")
+
+    def play(self):
+        possible_moves = list(self._game_reference.get_available_moves())
+        rand_item = possible_moves[random.randrange(len(possible_moves))]
+
+        self._game_reference.set_stone(rand_item)
