dcc5eba688e9557cf12237e213944bf580dc4c84
patrick.mueller <git-p@trickm.de>
2018-09-11 14:15:56 +0200

    refactor, add game_ends function

--- Development/PlayerAiForecastTurns.py
+++ Development/PlayerAiForecastTurns.py
@@ -2,6 +2,7 @@ from Player import Player
 from operator import itemgetter
 from OthelloGame import OthelloGame
 from Constants import BOARD_SIZE
+from StateForecastTree import StateForecastTree
 
 
 class PlayerAiForecastTurns(Player):
@@ -13,8 +14,10 @@ class PlayerAiForecastTurns(Player):
 
     def play(self):
 
-        new_othello = OthelloGame(BOARD_SIZE, True)
         old_board = self._game_reference.get_board()
+
+        new_othello = OthelloGame(len(old_board), True)
+
         new_board = OthelloGame.copy_board(old_board)
         new_board[0][0] = 0
         new_othello.set_board(new_board)
@@ -33,15 +36,15 @@ class PlayerAiForecastTurns(Player):
 
         tree.nodes[1].add_node(5, None)
 
-        print(tree.value)
+        print(tree.turn_number)
         print("++")
         tree.print_tree(tree.get_root_node())
 
-        tree = StateForecastTree.delete_parent(tree, tree.nodes[1])
+        tree = StateForecastTree.delete_parent(tree, tree.nodes[1].turn_number)
 
         print("Finished")
 
-        print(tree.value)
+        print(tree.turn_number)
         print("++")
         tree.print_tree(tree.get_root_node())
 
@@ -55,56 +58,3 @@ class PlayerAiForecastTurns(Player):
 
     def find_next_moves(self):
         pass
-
-
-class StateForecastTree:
-    def __init__(self, value, game_state, parent=None):
-        self.value = value
-        self.nodes = []
-        self.parent = parent
-        self.game_state = game_state
-
-    def delete(self, stop_node):
-        self.parent = None
-        if self is stop_node:
-            print("stop node reached" + str(self.value))
-            return self
-
-        val = None
-        for nodes in self.nodes:
-            val2 = nodes.delete(stop_node)
-            if val2 is not None:
-                val = val2
-        del self
-        return val
-
-    def add_node(self, node, game_state):
-        self.nodes.append(StateForecastTree(node, game_state, self))
-
-    def get_depth(self):
-        if self.parent is None:
-            return 1
-        return self.parent.get_depth() + 1
-
-    def delete_parent(self, actual_node):
-        if self.parent is not None:
-            return self.parent.delete_parent(actual_node)
-
-        # top level reached
-        return StateForecastTree.delete_nodes(self, actual_node)
-
-    def get_root_node(self):
-        if self.parent is not None:
-            return self.parent.get_root_node()
-        return self
-
-    @staticmethod
-    def print_tree(root):
-        for node in root.nodes:
-            print(node.value)
-            print("--")
-            StateForecastTree.print_tree(node)
-
-    @staticmethod
-    def delete_nodes(node, stop_node):
-        return node.delete(stop_node)
