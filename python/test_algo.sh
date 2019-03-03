3fd8b2f506cb9666b849e628bcb3bca2fdf1a957
patrick.mueller <git-p@trickm.de>
2019-03-03 14:21:45 +0100

    add script for automatic playing defined player configs

---
+++ python/test_algo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+wins_w=0;
+wins_b=0;
+for i in `seq 1 20`;
+	do
+		echo "round: "  $i
+		w1=$( python3 ./main-game.py <<< $(echo 2; sleep 1; echo 100; sleep 1; echo 3; sleep 1; echo 100 ) | grep -e "Winner")
+		for word in $w1; do
+			if [ $word == "W" ]; then
+				wins_w=$(echo  $wins_w + 1 | bc)
+			fi
+			if [ $word == "B" ]; then
+                                wins_b=$( echo $wins_b + 1 | bc)
+                        fi
+		done
+		echo $w1
+done
+echo "wins B: " $wins_b
+echo "wins W: " $wins_w
+w2=$(echo "(" $wins_b "*" 100 ") / ( " $wins_b "+" $wins_w ")" | bc )
+echo "B wins: " $w2 " %"
