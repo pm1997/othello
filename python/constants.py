b446d7270adc018c2592c0ef5cfef5f33a6ce61d
patrick.mueller <git@trickm.de>
2019-03-08 09:42:19 +0100

    add constants

---
+++ python/constants.py
@@ -0,0 +1,2 @@
+# Reverse of COLUMN_NAMES to match column name to a number
+COLUMN_NUMBERS = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8}
--- python/othello.py
+++ python/othello.py
@@ -20,6 +20,7 @@ class Othello:
     PLAYER_TWO = 2
     # Define the string used in print statements to represent a certain player or an empty field.
     PRINT_SYMBOLS = {EMPTY_CELL: " ", PLAYER_ONE: "B", PLAYER_TWO: "W", None: "None"}
+
     # Used to print the names of fields not like (1,1) but as (b,1) instead.
     COLUMN_NAMES = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h", 8: "i"}
 
