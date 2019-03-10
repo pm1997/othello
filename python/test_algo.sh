e5146afeb0128183b141f5379b9e514bae273921
patrick.mueller <git-p@trickm.de>
2019-03-10 18:32:26 +0100

    change ml algo to improved monte carlo

--- python/test_algo.sh
+++ python/test_algo.sh
@@ -5,7 +5,7 @@ wins_b=0;
 for i in `seq 1 20`;
 	do
 		echo "round: "  $i
-		w1=$( python3 ./main-game.py <<< $(echo 2; sleep 1; echo 100; sleep 1; echo 3; sleep 1; echo 100 ) | grep -e "Winner")
+		w1=$( python3 ./main-game.py <<< $(echo 4; sleep 1; echo 100; sleep 1; echo 4; sleep 1; echo 100 ) | grep -e "Winner")
 		for word in $w1; do
 			if [ $word == "W" ]; then
 				wins_w=$(echo  $wins_w + 1 | bc)
