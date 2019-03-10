5c965cc70afb28204be6a67b5a69f5af8eda01a2
epnigma <s162345@student.dhbw-mannheim.de>
2019-03-10 17:38:23 +0100

    Merge Player MonteCarlo and MonteCarlo2

--- python/players.py
+++ python/players.py
@@ -53,12 +53,15 @@ class PlayerMonteCarlo:
     The PlayerMonteCarlo determines the best move based on the simplest form of the MonteCarlo-Algorithm 
     """
 
+    start_tables = StartTables()
+
     def __init__(self):
         """
         Initialize the Player
         """
         # Ask the user to enter the number of random games per tur
         self.big_n = UtilMethods.get_integer_selection("Select Number of Simulated Games", 100, sys.maxsize)
+        self.use_start_lib = UtilMethods.get_boolean_selection("Do you want to use the start library?")
 
     @staticmethod
     def play_random_game(own_symbol, simulated_game):
@@ -84,6 +87,11 @@ class PlayerMonteCarlo:
         """
         Get the player's selection
         """
+        # Use start library if it is selected and still included
+        if self.use_start_lib and game_state.get_turn_nr() < 10:  # check whether start move match
+            moves = self.start_tables.get_available_start_tables(game_state)
+            if len(moves) > 0:
+                return UtilMethods.translate_move_to_pair(moves[random.randrange(len(moves))])
         # Create a dictionary to store information on won/lost ratios
         winning_statistics = dict()
         # Get the own symbol
@@ -115,38 +123,6 @@ class PlayerMonteCarlo:
         # Select the move with the maximum probability of winning
         selected_move = max(winning_statistics.items(), key=operator.itemgetter(1))[0]
         # Return the selected move
-        return selected_move
-
-
-class PlayerMonteCarlo2:
-
-    start_tables = StartTables()
-
-    def __init__(self):
-        self.big_n = UtilMethods.get_integer_selection("Select Number of Simulated Games", 100, sys.maxsize)
-
-    def get_move(self, game_state: Othello):
-        if game_state.get_turn_nr() < 10:  # check whether start move match
-            moves = self.start_tables.get_available_start_tables(game_state)
-            if len(moves) > 0:
-                return UtilMethods.translate_move_to_pair(moves[random.randrange(len(moves))])
-        winning_statistics = dict()
-        own_symbol = game_state.get_current_player()
-        possible_moves = game_state.get_available_moves()
-        for move in possible_moves:
-            winning_statistics[move] = (0, 1)  # set games played to 1 to avoid division by zero error
-
-        for i in range(self.big_n):
-            simulated_game = game_state.deepcopy()
-            first_played_move, won = PlayerMonteCarlo.play_random_game(own_symbol, simulated_game)
-            (won_games, times_played) = winning_statistics[first_played_move]
-            winning_statistics[first_played_move] = (won_games + won, times_played + 1)
-
-        for single_move in winning_statistics:
-            (games_won, times_played) = winning_statistics[single_move]
-            winning_statistics[single_move] = games_won / times_played
-
-        selected_move = max(winning_statistics.items(), key=operator.itemgetter(1))[0]
         return selected_move
 
 
