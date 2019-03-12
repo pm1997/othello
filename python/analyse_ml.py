334210c97d4d03762cfadc42ac33c29bb09458ad
patrick.mueller <git@trickm.de>
2019-03-12 15:24:46 +0100

    add coments, train ml with 20 games against random player

--- python/analyse_ml.py
+++ python/analyse_ml.py
@@ -36,27 +36,33 @@ class Analyse:
                 # print(a)
                 r += 1
 
+            # calculate first sum of array
             s1 = np.sum(self._data2[pos])
             print(f"max: {np.max(self._data2[pos])}")
             r = 0
             for row in self._data[position]:
+                # set unused moves to 100 %
                 if row[1] == 1:
                     a = 100
                     self._data2[pos][r // 8][r % 8] = "{:.2f}".format(a)
                 r += 1
             print(f"min: {np.min(self._data2[pos])}")
+
+            # calculate second sum of array
             s2 = np.sum(self._data2[pos])
-            s3 = (s1 + s2) / 128
-            print(f"average: {s3}")
+            # with both arrays, the error in average through unused moves (number of turns == 1), should be minimized
+            average = (s1 + s2) / 128
+            print(f"average: {average}")
 
             r = 0
             for row in self._data[position]:
+                # set unused moves to average value to minimize error in variance
                 if row[1] == 1:
-                    a = s3
-                    self._data2[pos][r // 8][r % 8] = "{:.2f}".format(a)
+                    self._data2[pos][r // 8][r % 8] = "{:.2f}".format(average)
                 r += 1
             print(f"var: {np.var(self._data2[pos], ddof=1)}")
 
+            # print array (board without column / row names)
             print(self._data2[pos])
 
             print("_______________________")
