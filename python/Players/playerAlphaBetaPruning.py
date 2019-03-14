7cd2e2751dd7b5e1d3ced7f7bb1a19490d550ca6
epnigma <s162345@student.dhbw-mannheim.de>
2019-03-14 20:48:11 +0100

    Restore working project

--- python/Players/playerAlphaBetaPruning.py
+++ python/Players/playerAlphaBetaPruning.py
@@ -92,7 +92,7 @@ class PlayerAlphaBetaPruning:
         if depth == 0:
             # use monte carlo player if enabled
             # ml_count = number of played games
-            ml = PlayerMonteCarlo(big_number=mc_count, use_start_libs=False, preprocessor_n=-1, heuristic=heuristic, use_multiprocessing=False)
+            ml = PlayerMonteCarlo(big_number=mc_count, use_start_libs=False, preprocessor_n=-1, heuristic=heuristic)
             # get best move
             move = ml.get_move(game_state)
             # return winnings stats of best move
