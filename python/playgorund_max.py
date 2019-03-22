79358b1e3f33b6bbf7085d00456a72e91bf5d10a
patrick.mueller <git@trickm.de>
2019-03-22 23:33:05 +0100

    add missing start tables, fix errors

--- python/playgorund_max.py
+++ python/playgorund_max.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     d = Database()
     d.sum_databases("database_moves_max.csv")
     # d.train_db_multi_threaded(100000)
-    d._store_database()
+    # d.store_database()
