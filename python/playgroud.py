1a714c83eb2bf535a785970905b0a25f00e8a664
patrick.mueller <git-p@trickm.de>
2019-03-22 21:23:04 +0100

    fix store db

--- python/playgroud.py
+++ python/playgroud.py
@@ -1,16 +1,17 @@
 from database import Database
-from start_tables import StartTables
-from othello import Othello
+# from start_tables import StartTables
+# from othello import Othello
 
 if __name__ == '__main__':
-    # db = Database()
-    # db.train_db_multi_threaded(10000)
+    db = Database()
+    db.train_db_multi_threaded(20000)
     # Trigger garbage collection
     # call destructor => store database to file
-    # db = None
-    g = Othello()
-    g.init_game()
-    st = StartTables()
+    db._store_database()
+    db = None
+    # g = Othello()
+    # g.init_game()
+    # st = StartTables()
     # st.calculate_missing_start_moves()
-    print(st.get_available_start_tables(g))
+    # print(st.get_available_start_tables(g))
 
