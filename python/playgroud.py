92942c34fccc992c35ff2998b557fc7c49b04bf5
patrick.mueller <git@trickm.de>
2019-03-22 13:20:07 +0100

    change start tables

--- python/playgroud.py
+++ python/playgroud.py
@@ -1,9 +1,16 @@
 from database import Database
+from start_tables import StartTables
+from othello import Othello
 
 if __name__ == '__main__':
-    db = Database()
-    db.train_db_multi_threaded(10000)
+    # db = Database()
+    # db.train_db_multi_threaded(10000)
     # Trigger garbage collection
     # call destructor => store database to file
-    db = None
+    # db = None
+    g = Othello()
+    g.init_game()
+    st = StartTables()
+    # st.calculate_missing_start_moves()
+    print(st.get_available_start_tables(g))
 
