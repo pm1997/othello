2bd2e747f552693d6aa4d6f3ee218c36ee17c629
epnigma <s162342@student.dhbw-mannheim.de>
2019-07-06 12:33:23 +0200

    Other statement for inverse dict

--- python/constants.py
+++ python/constants.py
@@ -9,10 +9,9 @@ PRINT_SYMBOLS = {EMPTY_CELL: " ", PLAYER_ONE: "B", PLAYER_TWO: "W", None: "None"
 COLUMN_NAMES = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h", 8: "i"}
 
 # Reverse of COLUMN_NAMES to match column name to a number
-COLUMN_NUMBERS = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8}
+COLUMN_NUMBERS = {name: number for number, name in COLUMN_NAMES.items()}
 
-# The move directions used to calculate the stones turned by a certain move
-# and the legality of that move respectively
+# The move directions used to calculate the stones turned by a certain move and the legality of that move respectively
 L = (-1, 0, 1)
 DIRECTIONS = {(a, b) for a in L for b in L if (a, b) != (0, 0)}
 
